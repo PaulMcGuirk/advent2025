81b892bf26220c3b36db952d643a3d80700fa891
paul <paul@fulcrumpro.com>
2025-12-08 08:09:53 -0600

    binary heap

--- rust/day08/src/main.rs
+++ rust/day08/src/main.rs
@@ -1,13 +1,28 @@
-use std::collections::HashSet;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashSet};
 use std::time::Instant;
 use std::{env, fs};
 
-struct Playground {
-    junctions: Vec<Vec<i64>>,
-    distances: Vec<(usize, usize, i64)>,
+#[derive(Eq, PartialEq)]
+struct RelativeVector {
+    i: usize,
+    j: usize,
+    dist_sq: i64,
 }
 
-fn read_and_parse_input() -> (Playground, usize) {
+impl Ord for RelativeVector {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.dist_sq.cmp(&self.dist_sq)
+    }
+}
+
+impl PartialOrd for RelativeVector {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn read_and_parse_input() -> (Vec<Vec<i64>>, usize) {
     let args = env::args().collect::<Vec<_>>();
 
     let filepath = &args[1];
@@ -25,107 +40,73 @@ fn read_and_parse_input() -> (Playground, usize) {
         })
         .collect::<Vec<_>>();
 
-    let distances = {
-        // can convert to a binary heap if needed
-        let mut distances = (0..junctions.len())
-            .flat_map(|i| {
-                ((i + 1)..junctions.len())
-                    .map(|j| {
-                        let dist_sq = (0..3)
-                            .map(|d| {
-                                let delta = junctions[i][d] - junctions[j][d];
-                                delta * delta
-                            })
-                            .sum::<i64>();
-                        (i, j, dist_sq)
-                    })
-                    .collect::<Vec<_>>()
-                    .into_iter()
-            })
-            .collect::<Vec<_>>();
-        distances.sort_by_key(|(_, _, dist_sq)| *dist_sq);
-        distances
-    };
-
-    let playground = Playground {
-        junctions,
-        distances,
-    };
-
-    (playground, pairs)
+    (junctions, pairs)
 }
 
-impl Playground {
-    fn solve_part_one(&self, pairs: usize) -> u64 {
-        let edges = {
-            let mut edges = vec![vec![]; self.junctions.len()];
-            for &(i, j, _) in self.distances.iter().take(pairs) {
-                edges[i].push(j);
-                edges[j].push(i);
-            }
-            edges
-        };
+fn solve(junctions: &Vec<Vec<i64>>, pairs: usize) -> (i64, i64) {
+    let mut distances = BinaryHeap::<RelativeVector>::new();
 
-        let mut seen = HashSet::new();
-        let mut pieces = vec![];
+    for i in 0..junctions.len() {
+        for j in (i + 1)..junctions.len() {
+            let dist_sq = (0..3)
+                .map(|d| {
+                    let delta = junctions[i][d] - junctions[j][d];
+                    delta * delta
+                })
+                .sum::<i64>();
 
-        for &(i, _, _) in self.distances.iter().take(pairs) {
-            if seen.contains(&i) {
-                continue;
-            }
-
-            let mut to_visit = vec![i];
-            let mut sub = vec![];
-
-            while let Some(next) = to_visit.pop() {
-                if seen.contains(&next) {
-                    continue;
-                }
-                seen.insert(next);
-                sub.push(next);
-
-                to_visit.extend(edges[next].clone());
-            }
-
-            pieces.push(sub);
+            let rel_vec = RelativeVector { i, j, dist_sq };
+            distances.push(rel_vec)
         }
-
-        pieces.sort_by_key(|sub| self.junctions.len() - sub.len());
-        pieces.iter().take(3).map(|sub| sub.len() as u64).product()
     }
 
-    fn solve_part_two(&self) -> i64 {
-        let mut contained_in = (0..self.junctions.len()).collect::<Vec<_>>();
-        let mut subsets: Vec<HashSet<usize>> = (0..self.junctions.len())
-            .map(|i| HashSet::from_iter([i].into_iter()))
-            .collect::<Vec<_>>();
+    let mut contained_in = (0..junctions.len()).collect::<Vec<_>>();
+    let mut subsets: Vec<Vec<usize>> = (0..junctions.len()).map(|i| vec![i]).collect::<Vec<_>>();
 
-        for &(i, j, _) in self.distances.iter() {
-            if contained_in[i] == contained_in[j] {
-                continue; // already in same set, can continue
-            }
+    let mut part_one: Option<i64> = None;
 
-            // merge j into i
-            let j_set_idx = contained_in[j];
-            let j_set = subsets[j_set_idx].clone();
-
-            let i_set_idx = contained_in[i];
-
-            for &k in j_set.iter() {
-                contained_in[k] = i_set_idx;
-            }
-
-            let i_set = subsets.get_mut(i_set_idx).unwrap();
-
-            i_set.extend(j_set);
-
-            if i_set.len() == self.junctions.len() {
-                return self.junctions[i][0] * self.junctions[j][0];
-            }
+    let mut edge_count = 0;
+    while let Some(rel_vec) = distances.pop() {
+        if edge_count == pairs {
+            let unique_subsets = contained_in.iter().cloned().collect::<HashSet<_>>();
+            let mut counts = unique_subsets
+                .iter()
+                .map(|set_idx| subsets[*set_idx].len() as i64)
+                .collect::<Vec<_>>();
+            counts.sort();
+            counts.reverse();
+            part_one = Some(counts.iter().take(3).product());
         }
 
-        panic!()
+        edge_count += 1;
+
+        let i = rel_vec.i;
+        let j = rel_vec.j;
+
+        if contained_in[i] == contained_in[j] {
+            continue;
+        }
+
+        let j_set_idx = contained_in[j];
+        let j_set = subsets[j_set_idx].clone();
+
+        let i_set_idx = contained_in[i];
+
+        for &k in j_set.iter() {
+            contained_in[k] = i_set_idx;
+        }
+
+        let i_set = subsets.get_mut(i_set_idx).unwrap();
+
+        i_set.extend(j_set);
+
+        if i_set.len() == junctions.len() {
+            let part_two = junctions[i][0] * junctions[j][0];
+            return (part_one.unwrap(), part_two);
+        }
     }
+
+    panic!()
 }
 
 fn main() {
@@ -134,10 +115,9 @@ fn main() {
 
     let now = Instant::now();
 
-    let (playground, pairs) = read_and_parse_input();
+    let (junctions, pairs) = read_and_parse_input();
 
-    let part_one = playground.solve_part_one(pairs);
-    let part_two = playground.solve_part_two();
+    let (part_one, part_two) = solve(&junctions, pairs);
 
     println!("Part one: {}", part_one);
     println!("Part two: {}", part_two);
